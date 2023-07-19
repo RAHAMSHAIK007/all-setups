3656fab12bc4063c4ae564b893241d8144ce8711
RAHAMSHAIK007 <81603735+RAHAMSHAIK007@users.noreply.github.com>
2023-07-19 18:55:59 +0530

    Create mysql.sh

---
+++ mysql.sh
@@ -0,0 +1,5 @@
+wget https://repo.percona.com/apt/percona-release_latest.$(lsb_release -sc)_all.deb
+sudo dpkg -i percona-release_latest.$(lsb_release -sc)_all.deb
+percona-release setup ps80
+sudo apt install percona-server-server
+mysql -u root -p
