e3e2287d03901624b2a3710002fdc139451da45f
RAHAMSHAIK007 <81603735+RAHAMSHAIK007@users.noreply.github.com>
2023-08-16 16:36:44 +0530

    Create terraform.sh

---
+++ terraform.sh
@@ -0,0 +1,4 @@
+sudo yum install -y yum-utils shadow-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
+aws configure
