efe4428b8386facdad36b1b4dee318515a97bc76
RAHAMSHAIK007 <81603735+RAHAMSHAIK007@users.noreply.github.com>
2023-08-18 09:34:47 +0530

    Create docker-compose.sh

---
+++ docker-compose.sh
@@ -0,0 +1,5 @@
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+ls /usr/local/bin/
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose version
