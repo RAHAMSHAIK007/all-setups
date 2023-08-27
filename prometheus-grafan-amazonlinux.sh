6f995dad5d566185ffe7e69a1ae4b796eb96241e
RAHAMSHAIK007 <81603735+RAHAMSHAIK007@users.noreply.github.com>
2023-08-27 21:36:11 +0530

    Update prometheus-grafan-amazonlinux.sh

--- prometheus-grafan-amazonlinux.sh
+++ prometheus-grafan-amazonlinux.sh
@@ -1,3 +1,4 @@
+#prometheus
 wget https://github.com/prometheus/prometheus/releases/download/v2.43.0/prometheus-2.43.0.linux-amd64.tar.gz
 tar -xf prometheus-2.43.0.linux-amd64.tar.gz
 sudo mv prometheus-2.43.0.linux-amd64/prometheus prometheus-2.43.0.linux-amd64/promtool /usr/local/bin
