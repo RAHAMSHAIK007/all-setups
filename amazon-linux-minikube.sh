64d3e7fff63d1c495013641825e23d33d7bdd865
RAHAMSHAIK007 <81603735+RAHAMSHAIK007@users.noreply.github.com>
2023-08-27 19:52:22 +0530

    Update amazon-linux-minikube.sh

--- amazon-linux-minikube.sh
+++ amazon-linux-minikube.sh
@@ -1,15 +1,12 @@
 yum install docker -y
-systemctl enable docker
 systemctl start docker
 systemctl status docker
-docker --version
-yum install conntrack -y
-yum install git -y
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo mv minikube-linux-amd64 /usr/local/bin/minikube
-sudo chmod +x /usr/local/bin/minikube
-sudo minikube version
-sudo minikube start --force --driver=docker
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo mv kubectl /usr/local/bin/kubectl
 sudo chmod +x /usr/local/bin/kubectl
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+sudo yum install iptables -y
+yum install conntrack -y
+minikube start --driver=docker --force
+minikube status 
