23122da8cf86a6e33a5883faf03ec4879f5ff441
RAHAMSHAIK007 <81603735+RAHAMSHAIK007@users.noreply.github.com>
2023-09-11 07:57:55 +0530

    Update sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -10,6 +10,5 @@ chmod 777 sonarqube-8.9.6.50800 -R
 su - sonar
 
 #run this on server manually
-sh /opt/sonarqube-8.9.6.50800/bin/linux/sonar.sh start
-
-#echo "user=admon & password=admin"
+#sh /opt/sonarqube-8.9.6.50800/bin/linux/sonar.sh start
+#echo "user=admin & password=admin"
