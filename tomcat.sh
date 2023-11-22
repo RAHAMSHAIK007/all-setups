6327a76c42a138f675d8c8addd77db74800ebb8b
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2023-11-22 09:41:02 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,10 +1,10 @@
 amazon-linux-extras install java-openjdk11 -y
-wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.82/bin/apache-tomcat-9.0.82.tar.gz
-tar -zxvf apache-tomcat-9.0.82.tar.gz
-sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.82/conf/tomcat-users.xml
-sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.82/conf/tomcat-users.xml
-sed -i '58  a\<user username="tomcat" password="raham123" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.82/conf/tomcat-users.xml
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.83/bin/apache-tomcat-9.0.83.tar.gz
+tar -zxvf apache-tomcat-9.0.83.tar.gz
+sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.83/conf/tomcat-users.xml
+sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.83/conf/tomcat-users.xml
+sed -i '58  a\<user username="tomcat" password="raham123" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.83/conf/tomcat-users.xml
 
-sed -i '21d' apache-tomcat-9.0.82/webapps/manager/META-INF/context.xml
-sed -i '22d'  apache-tomcat-9.0.82/webapps/manager/META-INF/context.xml
-sh apache-tomcat-9.0.82/bin/startup.sh
+sed -i '21d' apache-tomcat-9.0.83/webapps/manager/META-INF/context.xml
+sed -i '22d'  apache-tomcat-9.0.83/webapps/manager/META-INF/context.xml
+sh apache-tomcat-9.0.83/bin/startup.sh
