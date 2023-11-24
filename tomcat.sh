a03c5e35da9e22c31aa91c261462229ed460fcbf
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2023-11-24 16:29:52 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,6 +1,6 @@
 amazon-linux-extras install java-openjdk11 -y
 wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.83/bin/apache-tomcat-9.0.83.tar.gz
-tar -zxvf apache-tomcat-9.0.82.tar.gz
+tar -zxvf apache-tomcat-9.0.83.tar.gz
 sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.83/conf/tomcat-users.xml
 sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.83/conf/tomcat-users.xml
 sed -i '58  a\<user username="tomcat" password="raham123" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.83/conf/tomcat-users.xml
