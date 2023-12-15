aaaade846099a2ce6e16911b7250ba16c64b93ac
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2023-12-15 09:18:05 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -4,8 +4,8 @@ tar -zxvf apache-tomcat-9.0.83.tar.gz
 sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.83/conf/tomcat-users.xml
 sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.83/conf/tomcat-users.xml
 sed -i '58  a\<user username="tomcat" password="raham123" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.83/conf/tomcat-users.xml
-
+sed -i '59  a\</tomcat-users>' apache-tomcat-9.0.83/conf/tomcat-users.xml
+sed -i '56d' apache-tomcat-9.0.83/conf/tomcat-users.xml
 sed -i '21d' apache-tomcat-9.0.83/webapps/manager/META-INF/context.xml
 sed -i '22d'  apache-tomcat-9.0.83/webapps/manager/META-INF/context.xml
 sh apache-tomcat-9.0.83/bin/startup.sh
-
