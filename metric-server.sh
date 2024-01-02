d25913eee47a909ae73c3e6647e1b0ef426562a9
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2024-01-02 10:02:33 +0530

    Update metric-server.sh

--- metric-server.sh
+++ metric-server.sh
@@ -1,5 +1,9 @@
+FOR MINIKUBE:
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
-#minikube addons enable metrics-server #(only for minikube)
+minikube addons enable metrics-server #(only for minikube)
 
 kubectl top nodes
 kubectl top pods
+
+FOR KOPS:
+https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/high-availability-1.21+.yaml
