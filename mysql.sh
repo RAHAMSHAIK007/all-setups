5fb76b3f5d03b9596060584aa98d939f8aef5c8b
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2024-01-11 17:24:05 +0530

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,5 @@
 wget https://repo.percona.com/apt/percona-release_latest.$(lsb_release -sc)_all.deb
 sudo dpkg -i percona-release_latest.$(lsb_release -sc)_all.deb
 percona-release setup ps80
-sudo apt install percona-server-server
+sudo apt install percona-server-server -y
 mysql -u root -p
