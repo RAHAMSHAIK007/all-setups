c6492df172912d16fbcc2306e8be60e043be562d
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2024-02-22 21:01:25 +0530

    Update metric-server.sh

--- metric-server.sh
+++ metric-server.sh
@@ -6,4 +6,4 @@ kubectl top nodes
 kubectl top pods
 
 FOR KOPS:
-https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/high-availability-1.21+.yaml
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/high-availability-1.21+.yaml
