90bf7d3521bfd5789da9f0303776ee8cd825964a
dinu-359 <163173135+dinu-359@users.noreply.github.com>
2024-03-21 23:32:20 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -3,7 +3,7 @@ wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.86/bin/apache-tomcat-9.0.86.t
 tar -zxvf apache-tomcat-9.0.86.tar.gz
 sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.86/conf/tomcat-users.xml
 sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.86/conf/tomcat-users.xml
-sed -i '58  a\<user username="tomcat" password="raham123" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.86/conf/tomcat-users.xml
+sed -i '58  a\<user username="tomcat" password="dinesh" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.86/conf/tomcat-users.xml
 sed -i '59  a\</tomcat-users>' apache-tomcat-9.0.86/conf/tomcat-users.xml
 sed -i '56d' apache-tomcat-9.0.86/conf/tomcat-users.xml
 sed -i '21d' apache-tomcat-9.0.86/webapps/manager/META-INF/context.xml
