31b2a969b16eaa6e99505a692424dc9c6889b3a9
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2024-04-14 18:10:37 +0530

    Update swiggy.sh

--- swiggy.sh
+++ swiggy.sh
@@ -2,6 +2,6 @@ yum install httpd git -y
 systemctl start httpd
 systemctl status httpd
 cd /var/www/html
-git clone https://github.com/in-imitable/Swiggy-Clone.git
+git clone https://github.com/karishma1521success/swiggy-clone.git
 mv Swiggy-Clone/* .
 tail -100f /var/log/httpd/access_log
