536d433ca8fc8b518500974033934c0cb775eb65
dinu-359 <163173135+dinu-359@users.noreply.github.com>
2024-04-24 13:25:35 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -11,7 +11,7 @@ chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
 
-aws s3api create-bucket --bucket cloudanddevopsbyraham0073456.k8s.local --region us-east-1
+aws s3api create-bucket --bucket dinesh4a9.devops --region us-east-1
 aws s3api put-bucket-versioning --bucket cloudanddevopsbyraham0073456.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
 export KOPS_STATE_STORE=s3://cloudanddevopsbyraham0073456.k8s.local
 kops create cluster --name rahams.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.medium
