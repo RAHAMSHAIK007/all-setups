9fc74e417f78a035e310ef59d3e9d31ed8fa3db2
dinu-359 <163173135+dinu-359@users.noreply.github.com>
2024-05-27 22:14:25 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,11 +1,12 @@
 amazon-linux-extras install java-openjdk11 -y
-wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.24/bin/apache-tomcat-10.1.24.tar.gz
-tar -zxvf apache-tomcat-9.0.86.tar.gz
-sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.86/conf/tomcat-users.xml
-sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.86/conf/tomcat-users.xml
-sed -i '58  a\<user username="tomcat" password="dinesh" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.86/conf/tomcat-users.xml
-sed -i '59  a\</tomcat-users>' apache-tomcat-9.0.86/conf/tomcat-users.xml
-sed -i '56d' apache-tomcat-9.0.86/conf/tomcat-users.xml
-sed -i '21d' apache-tomcat-9.0.86/webapps/manager/META-INF/context.xml
-sed -i '22d'  apache-tomcat-9.0.86/webapps/manager/META-INF/context.xml
-sh apache-tomcat-9.0.86/bin/startup.sh
+wget https://dlcdn.apache.org/tomcat/tomcat-11/v11.0.0-M20/bin/apache-tomcat-11.0.0-M20.tar.gz
+tar -zxvf apache-tomcat-11.0.0-M20.tar.gz
+mv apache-tomcat-11.0.0-M20 tomcat
+sed -i '56  a\<role rolename="manager-gui"/>' tomcat/conf/tomcat-users.xml
+sed -i '57  a\<role rolename="manager-script"/>' tomcat/conf/tomcat-users.xml
+sed -i '58  a\<user username="tomcat" password="dinesh" roles="manager-gui, manager-script"/>' tomcat/conf/tomcat-users.xml
+sed -i '59  a\</tomcat-users>' tomcat/conf/tomcat-users.xml
+sed -i '56d' tomcat/conf/tomcat-users.xml
+sed -i '21d' tomcat/webapps/manager/META-INF/context.xml
+sed -i '22d'  tomcat/webapps/manager/META-INF/context.xml
+sh tomcat/bin/startup.sh
