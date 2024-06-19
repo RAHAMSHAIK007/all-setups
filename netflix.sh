8ba5b2f916a299ad5a762d8e42c5a726600be11e
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2024-06-19 18:38:36 +0530

    Update netflix.sh

--- netflix.sh
+++ netflix.sh
@@ -1,6 +1,7 @@
 yum install httpd git -y
 systemctl start httpd
 systemctl status httpd
+chkconfig httpd on
 cd /var/www/html
 git clone https://github.com/CleverProgrammers/pwj-netflix-clone.git
 mv pwj-netflix-clone/* .
