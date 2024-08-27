ee83a2d1298201c7f00b5008d7327f812347a2de
MUSTAFA SHAIK <106308110+devops0014@users.noreply.github.com>
2024-08-27 07:26:23 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,5 +1,5 @@
 amazon-linux-extras install java-openjdk11 -y
-wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.91/bin/apache-tomcat-9.0.91.tar.gz
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.93/bin/apache-tomcat-9.0.93.tar.gz
 tar -zxvf apache-tomcat-9.0.91.tar.gz
 sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.91/conf/tomcat-users.xml
 sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.91/conf/tomcat-users.xml
