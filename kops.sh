8ff16d44d57897f1164207c52fd7df2a459f8144
Shaikh Zameer9130 <158508224+shaikhzameer@users.noreply.github.com>
2024-08-29 21:20:34 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -14,5 +14,5 @@ mv kops-linux-amd64 /usr/local/bin/kops
 aws s3api create-bucket --bucket cloudanddevopsbzameeram0073456.k8s.local --region us-east-1
 aws s3api put-bucket-versioning --bucket cloudanddevopsbyrzameer0073456.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
 export KOPS_STATE_STORE=s3://cloudanddevopsbyrzameer0073456.k8s.local
-kops create cluster --name zameer.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.medium
-kops update cluster --name zameer.k8s.local --yes --admin
+kops create cluster --name zameers.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.medium
+kops update cluster --name zameers.k8s.local --yes --admin
