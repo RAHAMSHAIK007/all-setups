3a70f96f86e247b76329e3daf2e31a4b4aa19fe7
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2024-09-19 09:53:40 +0530

    Create whatsapp.sh

---
+++ whatsapp.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+yum install httpd git -y
+systemctl start httpd
+systemctl status httpd
+chkconfig httpd on
+cd /var/www/html
+git clone  https://github.com/hamsahmedansari/axiom-whatsapp-ui-homePage.git
+mv axiom-whatsapp-ui-homePage.git/* .
+tail -100f /var/log/httpd/access_log
