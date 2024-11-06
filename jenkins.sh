bc478ae971ea83392b9ca825ee332e1afbdeabfe
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2024-11-06 11:14:19 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -6,7 +6,7 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 
 #STEP-3: DOWNLOAD JAVA11 AND JENKINS
-amazon-linux-extras install java-openjdk11 -y
+sudo yum install java-17-amazon-corretto -y
 yum install jenkins -y
 update-alternatives --config java
 
