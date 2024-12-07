274bea7351a7ceaf966680485f3a78d93321a85c
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2024-12-07 16:48:51 +0530

    Update jfrog.sh

--- jfrog.sh
+++ jfrog.sh
@@ -4,4 +4,4 @@ sudo yum install java-1.8* -y
 sudo wget https://jfrog.bintray.com/artifactory/jfrog-artifactory-oss-6.9.6.zip
 sudo yum install unzip -y
 sudo unzip  jfrog-artifactory-oss-6.9.6.zip
-sudo sh jfrog-artifactory-oss-6.9.6/app/bin/artifactory.sh start
+sudo sh jfrog-artifactory-oss-6.9.6/bin/artifactory.sh start
