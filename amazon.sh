415b3b1b778e249d6ef0573117fc9017b4024208
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2024-12-15 20:58:28 +0530

    Update amazon.sh

--- amazon.sh
+++ amazon.sh
@@ -5,4 +5,4 @@ systemctl status httpd
 cd /var/www/html
 git clone https://github.com/Ironhack-Archive/online-clone-amazon.git
 mv online-clone-amazon/* .
-tail -100f /var/log/httpd/access_log
+tail -f /var/log/httpd/access_log
