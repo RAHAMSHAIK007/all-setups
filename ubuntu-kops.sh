0d4c3f8d05d73e57b33390c71fd3850fdef227e0
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2025-08-13 18:39:43 +0530

    Update ubuntu-kops.sh

--- ubuntu-kops.sh
+++ ubuntu-kops.sh
@@ -10,7 +10,7 @@ unzip awscliv2.zip
 sudo ./aws/install
 aws configure
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-wget https://github.com/kubernetes/kops/releases/download/v1.25.0/kops-linux-amd64
+wget https://github.com/kubernetes/kops/releases/download/v1.33.0/kops-linux-amd64
 chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
