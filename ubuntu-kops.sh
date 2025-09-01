969f614a483b5c2842b7f89a6c258b47efc1c1c1
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2025-09-01 19:12:34 +0530

    Update ubuntu-kops.sh

--- ubuntu-kops.sh
+++ ubuntu-kops.sh
@@ -15,8 +15,8 @@ chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
 
-aws s3api create-bucket --bucket deploysscloudanddevopsbyraham007899123.k8s.local --region us-east-1
-aws s3api put-bucket-versioning --bucket deploysscloudanddevopsbyraham007899123.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://deploysscloudanddevopsbyraham007899123.k8s.local
-kops create cluster --name rahamss.k8s.local --zones ap-south-1a --control-plane-image ami-0f918f7e67a3323f0 --control-plane-count=1 --control-plane-size t2.medium --image ami-0f918f7e67a3323f0 --node-count=2 --node-size t2.micro
+aws s3api create-bucket --bucket deploysscloudanddevopsbyraham007899123mnbvcxz.k8s.local --region us-east-1
+aws s3api put-bucket-versioning --bucket deploysscloudanddevopsbyraham007899123mnbvcxz.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
+export KOPS_STATE_STORE=s3://deploysscloudanddevopsbyraham007899123mnbvcxz.k8s.local
+kops create cluster --name rahamss.k8s.local --zones us-east-1a --control-plane-image ami-0360c520857e3138f --control-plane-count=1 --control-plane-size t2.large --image ami-0360c520857e3138f --node-count=2 --node-size t2.medium
 kops update cluster --name rahams.k8s.local --yes --admin
