24cd3f8caaff9c54e48c585c15b13d6956d9ae87
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2025-10-15 19:55:14 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,15 +1,11 @@
 
 #! /bin/bash
-sudo apt update -y
-sudo apt install -y openjdk-17-jdk
-java -version
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt update -y
-sudo apt install -y jenkins
-sudo systemctl start jenkins
-sudo systemctl status jenkins
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+# Add required dependencies for the jenkins package
+yum install java-17-amazon-corretto jenkins -y
+sudo yum install jenkins -y
+sudo systemctl restart jenkins
+sudo systemctl daemon-reload
