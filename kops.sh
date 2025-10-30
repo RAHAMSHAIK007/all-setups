b4a821802afb47198452d644a8fa863dd5500b22
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2025-10-30 19:38:48 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -1,6 +1,4 @@
-#vim .bashrc
-#export PATH=$PATH:/usr/local/bin/
-#source .bashrc
+echo "export PATH=$PATH:/usr/local/bin/" >> source .bashrc
 
 
 #! /bin/bash
@@ -11,8 +9,6 @@ chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
 
-aws s3api create-bucket --bucket cloudanddevopsbyraham0073456.k8s.local --region us-east-1
-aws s3api put-bucket-versioning --bucket cloudanddevopsbyraham0073456.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
 export KOPS_STATE_STORE=s3://cloudanddevopsbyraham0073456.k8s.local
 kops create cluster --name rahamss.k8s.local --zones ap-south-1a --control-plane-image ami-0f918f7e67a3323f0 --control-plane-count=1 --control-plane-size t2.large --image ami-0f918f7e67a3323f0 --node-count=2 --node-size t2.medium
 kops update cluster --name rahams.k8s.local --yes --admin
