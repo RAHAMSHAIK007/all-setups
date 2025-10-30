97e686689d222e75460e4d792be377db44254caf
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2025-10-30 20:43:10 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -1,6 +1,3 @@
-echo "export PATH=$PATH:/usr/local/bin/" >> source .bashrc
-
-
 #! /bin/bash
 aws configure
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
@@ -8,6 +5,7 @@ wget https://github.com/kubernetes/kops/releases/download/v1.33.0/kops-linux-amd
 chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
+echo "export PATH=$PATH:/usr/local/bin/" >> source .bashrc
 
 export KOPS_STATE_STORE=s3://cloudanddevopsbyraham0073456.k8s.local
 kops create cluster --name rahamss.k8s.local --zones ap-south-1a --control-plane-image ami-0f918f7e67a3323f0 --control-plane-count=1 --control-plane-size t2.large --image ami-0f918f7e67a3323f0 --node-count=2 --node-size t2.medium
