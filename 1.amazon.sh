f35a0d24f5891075188ce85a3a5679b5e4c11bd3
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2025-11-04 16:43:09 +0530

    Update 1.amazon.sh

--- 1.amazon.sh
+++ 1.amazon.sh
@@ -3,4 +3,4 @@ sudo -i
 apt update
 apt install apache2  -y
 git clone https://github.com/Ironhack-Archive/online-clone-amazon.git
-mv online-clone-amazon/* /var/www/html
+mv online-clone-amazon/* /var/www/html/
