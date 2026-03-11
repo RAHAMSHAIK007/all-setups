44aeb29663be424a14854fb9635282a01b22fa5c
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2026-03-11 06:55:55 +0530

    Update ubuntu-jenkins.sh

--- ubuntu-jenkins.sh
+++ ubuntu-jenkins.sh
@@ -2,13 +2,14 @@
 sudo apt update -y
 sudo apt install -y openjdk-17-jdk
 java -version
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt update -y
-sudo apt install -y jenkins
+sudo apt update
+sudo apt install jenkins
+
 sudo systemctl start jenkins
 sudo systemctl status jenkins
