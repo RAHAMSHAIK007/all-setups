d9f4215a56bc6dab893dcfca0dff66dd3bd1e199
Anirudh Talluri <168444331+anitalluri00@users.noreply.github.com>
2026-07-19 09:08:21 +0530

    Update sonar.sh for SonarQube installation steps

--- sonar.sh
+++ sonar.sh
@@ -1,14 +1,31 @@
-#! /bin/bash
-#Launch an instance with 9000 and t2.medium
+#!/bin/bash
+# Launch an instance with port 9000 open and t2.medium or t3.medium
+# OS: Amazon Linux 2023 (Kernel 6.1 or 6.1.x)
+
+# 1. Install prerequisites
+dnf update -y
+dnf install wget unzip -y
+
+# 2. Install Java 17 (Required for SonarQube 9.9+)
+dnf install java-17-amazon-corretto -y
+
+# 3. Increase OS limits for Elasticsearch (Required, or SonarQube will crash)
+echo "vm.max_map_count=262144" >> /etc/sysctl.conf
+echo "fs.file-max=131072" >> /etc/sysctl.conf
+sysctl -p
+
+# 4. Download and extract SonarQube
 cd /opt/
 wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.9.6.92038.zip
 unzip sonarqube-9.9.6.92038.zip
-amazon-linux-extras install java-openjdk11 -y
-useradd sonar
-chown sonar:sonar sonarqube-9.9.6.92038 -R
-chmod 777 sonarqube-9.9.6.92038 -R
-su - sonar
+mv sonarqube-9.9.6.92038 sonarqube
 
-#run this on server manually
-#sh /opt/sonarqube-9.9.6.92038/bin/linux/sonar.sh start
-#echo "user=admin & password=admin"
+# 5. Create user and fix permissions
+useradd sonar
+chown -R sonar:sonar /opt/sonarqube
+
+# 6. Start SonarQube as the sonar user (non-interactive)
+su - sonar -c "/opt/sonarqube/bin/linux-x86-64/sonar.sh start"
+
+echo "SonarQube is starting. Note: It may take 2-3 minutes for the web UI to become available."
+echo "Default credentials -> user=admin & password=admin"
