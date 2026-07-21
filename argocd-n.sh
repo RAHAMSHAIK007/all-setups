ee48977b289f2d61d8407c8d08240707f0115087
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2026-07-22 00:23:06 +0530

    Update argocd-n.sh

--- argocd-n.sh
+++ argocd-n.sh
@@ -12,7 +12,7 @@ sudo chown $USER:$USER ~/.kube/config
 kubectl get pods -A
 
 sleep 10
-kubectl create namespace argocd
+
 kubectl create namespace argocd
 kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl get pods -n argocd
